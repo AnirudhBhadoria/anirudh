7f620a2de52e8155edaaa6f20c1abcc2fa3a009e
AnirudhBhadoria <147118213+AnirudhBhadoria@users.noreply.github.com>
2023-10-06 07:22:18 +0530

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,15 @@
+name=input("Enter the name \n")
+print(name,"is the name of user ")
+
+#2 Problem-
+
+firstname=input("Enter your first name\n")
+lastname=input("Enter your last name\n")
+
+print(firstname,lastname,sep='-',end='\t')
+
+#3Problem
+
+username=input("Enter name\n")
+print("Welcome to Pramod's territory",username)
+
